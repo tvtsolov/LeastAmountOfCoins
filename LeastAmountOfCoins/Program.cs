c8e0ea602964957ad584dcca1bd81d6b4f3b9917
tvtsolov <65929968+tvtsolov@users.noreply.github.com>
2023-01-05 09:17:20 +0200

    Update Program.cs

--- LeastAmountOfCoins/Program.cs
+++ LeastAmountOfCoins/Program.cs
@@ -7,115 +7,85 @@ class Program
 {
     static void Main()
     {
-        
-        decimal  coins50 = 0;
-        int  coins20 = 0;
-        int  coins10 = 0;
-        int  coins5 = 0;
-        int  coins2 = 0;
-        int  coins1 = 0;
-        decimal  price = Convert.ToDecimal(Console.ReadLine());
-        decimal  paid = Convert.ToDecimal(Console.ReadLine());
-        
-        decimal  change = paid - price;
+
+        int coins50 = 0;
+        int coins20 = 0;
+        int coins10 = 0;
+        int coins5 = 0;
+        int coins2 = 0;
+        int coins1 = 0;
+        decimal price = Convert.ToDecimal(Console.ReadLine());
+        decimal paid = Convert.ToDecimal(Console.ReadLine());
+
+        decimal change = paid - price;
         int leva = Convert.ToInt32(Math.Floor(change));
 
-        Console.WriteLine($"Change normal: {change}");
-        Console.WriteLine($"change modulo: {change % 1}");
-
-        change = (change % 1); //0.51
-
-        
+        change = (change % 1);
 
         if (change > 0)
         {
-            coins50 = change / 0.5m;
-            Console.WriteLine($"50 stotinki: {coins50}");
+            coins50 = (int)((change % 1) / 0.5m);
+            decimal remainder = (change % 0.5m);
 
-            if ((coins50 % 1) > 0 && (coins50 % 1) >= 0.2m)
+            if ((remainder % 1) > 0 && (remainder % 1) >= 0.2m)
             {
-                coins20 = (int)((coins50 % 1) / 0.2m);
-            } else if ((coins50 % 1) > 0 && (coins50 % 1) < 0.2m && (coins50 % 1) >= 0.1m)
+                coins20 = (int)((remainder % 1) / 0.2m);
+                remainder = (remainder - coins20 * 0.2m);
+            }
+            if (remainder != 0 && remainder < 0.2m && remainder >= 0.1m)
             {
-                coins10 = (int)((coins50 % 1) / 0.1m);
-            } else if ((coins50 % 1) > 0 && (coins50 % 1) < 0.2m && (coins50 % 1) < 0.1m && (coins50 % 1) >= 0.05m)
+                coins10 = (int)((remainder % 1) / 0.1m);
+                remainder = (remainder - coins10 * 0.1m);
+            }
+            if (remainder != 0 && remainder < 0.2m && remainder < 0.1m && remainder % 1 >= 0.05m)
             {
-                coins5 = (int)((coins50 % 1) / 0.05m);
-            } else if ((coins50 % 1) > 0 && (coins50 % 1) < 0.2m && (coins50 % 1) < 0.1m && (coins50 % 1) < 0.05m && (coins50 % 1) >= 0.02m)
+                coins5 = (int)((remainder % 1) / 0.05m);
+                remainder = (remainder - coins5 * 0.05m);
+            }
+            if (remainder != 0 && remainder < 0.2m && remainder < 0.1m && remainder < 0.05m && remainder >= 0.02m)
             {
-                coins2 = (int)((coins50 % 1) / 0.02m);
-            } else if ((coins50 % 1) > 0 && (coins50 % 1) < 0.2m && (coins50 % 1) < 0.1m && (coins50 % 1) < 0.05m && (coins50 % 1) < 0.02m && (coins50 % 1) >= 0.01m)
+                coins2 = (int)((remainder % 1) / 0.02m);
+                remainder = (remainder - coins2 * 0.02m);
+            }
+            if (remainder != 0 && remainder < 0.2m && remainder % 1 < 0.1m && remainder % 1 < 0.05m && remainder < 0.02m && remainder % 1 >= 0.01m)
             {
-                coins1 = (int)((coins50 % 1) / 0.01m);
-            } 
-
-
+                coins1 = (int)((remainder % 1) / 0.01m);
+            }
         }
-        Console.WriteLine($"Change is: {change}");
-        Console.WriteLine($"Coins in leva: {leva}");
-        Console.WriteLine($"Coins in .50 is: {coins50:F0}");
-        Console.WriteLine($"Coins in .20 is: {coins20:F0}");
-        Console.WriteLine($"Coins in .10 is: {coins10:F0}");
-        Console.WriteLine($"Coins in .5 is: {coins5:F0}");
-        Console.WriteLine($"Coins in .2 is: {coins2:F0}");
-        Console.WriteLine($"Coins in .1 is: {coins1:F0}");
 
-
-
-
-        //if (change > 0)
-        //{
-        //    coins50 = change / 0.5;
-        //}
-
-        // Console.WriteLine($"Change: {change}");
-
-
-
-
-
-        //Console.WriteLine($"50 st: {coins50} change {change}");
-
-
-
+        //Output
+     
+        if (leva >= 1)
+        {
+            Console.WriteLine($"{leva:F0} x 1 lev");
+        }
+        if (coins50 >= 1)
+        {
+            Console.WriteLine($"{coins50:F0} x 50 stotinki");
+        }
+        if (coins20 >= 1)
+        {
+            Console.WriteLine($"{coins20:F0} x 20 stotinki");
+        }
+        if (coins10 >= 1)
+        {
+            Console.WriteLine($"{coins10:F0} x 10 stotinki");
+        }
+        if (coins5 >= 1)
+        {
+            Console.WriteLine($"{coins5:F0} x 5 stotinki");
+        }
+        if (coins2 >= 1)
+        {
+            Console.WriteLine($"{coins2:F0} x 2 stotinki");
+        }
+        if (coins1 >= 1)
+        {
+            Console.WriteLine($"{coins1:F0} x 1 stotinka");
+        }
     }
-}
+} 
 
-
-
-
-
-/*
-double coins50 = coins / 0.5;
-double coins20 = coins / 0.2;
-double coins10 = coins / 0.1;
-double coins5 = coins / 0.05;
-double coins2 = coins / 0.02;
-double coins1 = coins / 0.01;
-*/
-
-//int coins50 = (int)(change / 0.50);
-
-
-/*
-Console.WriteLine($"Change is: {change}");
-Console.WriteLine($"Coins in leva: {leva}");
-Console.WriteLine($"Coins in .50 is: {coins50:F0}");
-Console.WriteLine($"Coins in .20 is: {coins20:F0}");
-Console.WriteLine($"Coins in .10 is: {coins10:F0}");
-Console.WriteLine($"Coins in .5 is: {coins5:F0}");
-Console.WriteLine($"Coins in .2 is: {coins2:F0}");
-Console.WriteLine($"Coins in .1 is: {coins1:F0}");
-
- */
-
-
-
-
-
-//  price 4.49
-//  paid 7
-//  change is = 2.51
+// notes notes
 //
-//  result:
-//  1, 1, 50, 0.01
+
